8d379b76783f8495f7b46928651b56b44eed7891
Jordan Hatch <jordan@jordanh.net>
2012-04-30 15:17:39 +0000

    Add root page for Paul

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,9 @@
+class RootController < ApplicationController
+
+  # there's no magic here
+
+  def index
+    # load index.html.erb
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Migratorator::Application.routes.draw do
   get 'mappings/find' => 'mappings#show', :as => :find_mapping
   resources :mappings
 
-  root :to => "mappings#index"
+  root :to => "root#index"
 
 end

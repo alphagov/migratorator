3413c87e4a969351fd87ae2da86213f6b1126e54
Jordan Hatch <jordan@jordanh.net>
2012-05-02 12:10:18 +0000

    Update jenkins.sh to drop the Mongo db and to support webkit_server

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 export RAILS_ENV=test
+export DISPLAY=":99"
+
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
+bundle exec rake db:mongoid:drop
 bundle exec rake db:schema:load
 bundle exec rake

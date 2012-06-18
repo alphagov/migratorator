44491648eeb45f646c54788b7292353e02a8cc83
Jordan Hatch <jordan@jordanh.net>
2012-06-18 15:56:23 +0000

    Add further progress bars (for all four current sources) to dashboard

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -9,6 +9,8 @@ class RootController < ApplicationController
     @overall_progress = Mapping.progress([], Tag::STATUS_DONE_TAG)
     @friendly_url_progress = Mapping.progress(["source:aliases"], Tag::STATUS_DONE_TAG)
     @mapping_exercise_progress = Mapping.progress(["source:mapping-exercise"], Tag::STATUS_DONE_TAG)
+    @crawler_progress = Mapping.progress(["source:crawler"], Tag::STATUS_DONE_TAG)
+    @apache_logs_progress = Mapping.progress(["source:apache-logs"], Tag::STATUS_DONE_TAG)
   end
 
 end

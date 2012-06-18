d04f17a6b386b609d81cac75bd808ea749c95df6
Jordan Hatch <jordan@jordanh.net>
2012-06-18 14:29:25 +0000

    Refactored progress bar and added additional bars to the dashboard

--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -5,7 +5,10 @@ class RootController < ApplicationController
   def index
     # load index.html.erb
     @revisions = HistoryTracker.limit(15).all
-    @progress = Mapping.progress([], Tag::STATUS_DONE_TAG)
+
+    @overall_progress = Mapping.progress([], Tag::STATUS_DONE_TAG)
+    @friendly_url_progress = Mapping.progress(["source:aliases"], Tag::STATUS_DONE_TAG)
+    @mapping_exercise_progress = Mapping.progress(["source:mapping-exercise"], Tag::STATUS_DONE_TAG)
   end
 
 end
--- app/helpers/mappings_helper.rb
+++ app/helpers/mappings_helper.rb
@@ -6,4 +6,8 @@ module MappingsHelper
   def referer_session
     @referer_session ||= (session[referer_session_name(resource.id)] || {}).select {|k,v| [:page, :tags].include?(k) }
   end
+
+  def progress_bar_for(data)
+    render :partial => 'mappings/progress_bar', :locals => { :progress => data }
+  end
 end

6072c47a122e85ea74accf7b1f597d460e004349
Jordan Hatch <jordan@jordanh.net>
2012-06-26 09:54:48 +0000

    Reinstate yesterdays API commits.
    This reverts commit 3b64decb70ab5fa0a2a161c410357cc4eeeda7c4.

--- spec/controllers/mapping_controller_spec.rb
+++ spec/controllers/mapping_controller_spec.rb
@@ -45,45 +45,6 @@ describe MappingsController do
     end
   end
 
-  describe "when retrieving a mapping" do
-    describe "JSON" do
-      before do
-        @mapping = Mapping.create! :title => "An example redirect", :old_url  => 'http://example.com/a_long_uri', :new_url => 'http://gov.uk/test', :status => 301, :tags => ["section:education", "article", "need-met:y"], :notes => "A note string"
-        @json_representation = {
-          "mapping" => {
-            "id"           => @mapping.id,
-            "title"         => "An example redirect",
-            "old_url"       => @mapping.old_url,
-            "status"        => 301,
-            "new_url"       => @mapping.new_url,
-            "notes"         => "A note string",
-            "search_query"  => nil,
-            "tags"          => ["article", "need-met:y", "section:education"]
-          }
-        }.to_json
-      end
-
-      it "should return a JSON object for a valid mapping" do
-        get :show, old_url: @mapping.old_url, format: 'json'
-
-        response.should be_success
-        response.body.should == @json_representation
-      end
-
-      it "should return a 404 error for an invalid mapping" do
-        get :show, old_url: "http://example.com/blah", format: 'json'
-
-        response.status.should == 404
-      end
-
-      it "should return a 400 Bad Request error for an invalid mapping" do
-        get :show
-
-        response.status.should == 400
-      end
-    end
-  end
-
 
   describe "when creating a mapping" do
 

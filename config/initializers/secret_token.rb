ff44340272ae029e85e7db47c3b17f4e22a7751b
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-07-06 16:37:22 +0100

    Fix inability to signin in dev mode

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Migratorator::Application.config.secret_token = '44910bb8f9eaffcf09eef8ee5a6789c035b6ce53400a28b461747c12541a2a351a684af4242fd6313db9bc5fcb65bb99c8995d76e143903ad42e2baee0139492'
+Migratorator::Application.config.secret_token = 'b44910bb8f9eaffcf09eef8ee5a6789c035b6ce53400a28b461747c12541a2a351a684af4242fd6313db9bc5fcb65bb99c8995d76e143903ad42e2baee0139492'

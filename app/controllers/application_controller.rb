9d27a43bcd214e474221b59c38d0f665913b6f3b
i0n <ianalexanderwood@gmail.com>
2012-08-03 17:21:36 +0100

    added application controller method to test for csv format and edited before filters to skip authentication on a csv request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
 
   include GDS::SSO::ControllerMethods
 
-  before_filter :authenticate_user!
-  before_filter :require_signin_permission!
+  before_filter :authenticate_user!, :unless => :format_is_csv?
+  before_filter :require_signin_permission!, :unless => :format_is_csv?
+
+  def format_is_csv?
+    request.format.csv? 
+  end
+
 end
